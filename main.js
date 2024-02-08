7d90a8a04ffc309b8fd0bbe3d2903c7157acaae2
VishiGotDrip <108681366+VishiGotDrip@users.noreply.github.com>
2024-02-08 17:32:57 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,25 @@
+Webcam.set({
+    width:350,
+    height:265,
+    image_format:'png',
+    png_quality:90
+});
+
+camera=document.getElementById("camera");
+Webcam.attach('#camera');
+
+function capture()
+{
+    Webcam.snap(function (data_uri)
+    {
+        document.getElementById("result").innerHTML='<img id="captured_image" src="'+data_uri+'"/>';
+    });
+}
+
+console.log('ml5version', ml5.version);
+classifier=ml5.imageClassifier('', modelLoaded);
+
+function modelLoaded()
+{
+    console.log('modelLoaded')
+}
